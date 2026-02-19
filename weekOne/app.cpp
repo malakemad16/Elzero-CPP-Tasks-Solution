8e14aeab353303946cd9f866d1548c82efe77424
malakemad16 <maea12900@gmail.com>
2026-02-19 14:41:55 +0200

    done week one

---
+++ weekOne/app.cpp
@@ -0,0 +1,68 @@
+// التكليف 01
+#include <iostream> //1- the # before include 2-the iostream is library ame can't have ""
+
+int main() // 3- missing the return type for the function 4- "main" case sensetive
+{
+  std::cout << "Line One\n"; // 5- Syntax errr 
+  std::cout << "Line Two\n"; // 6- Syntax errr 
+  std::cout << "Line Three\n"; // 7- Syntax errr 
+  std::cout << "===============\n"; 
+  
+  // التكليف 02
+/*
+  iostream Header File Content
+  -- cout
+  -- cin
+  -- cerr
+  -- clog
+  -- wcout
+  -- wcin
+  -- wcerr
+  -- wclog
+*/
+
+// التكليف 03
+std::cout << "Iam Learning Programming\nwith\nElzero\nWeb\nSchool\n<3";
+
+/* output
+Iam Learning Programming
+With
+Elzero
+Web
+School
+<3 */
+std::cout << "\n===============\n"; 
+
+//  التكليف 04
+std::cout << "|=======================|\n\
+|== Elzero Web School ==|\n\
+|==        C++        ==|\n\
+|=======================|";
+
+/*
+output
+|=======================|
+|== Elzero Web School ==|
+|==        C++        ==|
+|=======================|
+*/
+std::cout << "\n===============\n"; 
+
+// Note: to remove the comment from التكليف 05 comment all the code
+
+// //  التكليف 05=> comments
+// // This Is My First File
+// // In Learning Programming
+// // Iam Happy <3
+// // Iam Dragon
+
+// #include <iostream> // Preprocessor Directive 
+
+// int main()
+// {
+//   std::cout /*Character Output*/  << "Line One\n";
+//   std::cout << "Line Two\n"; // cout Function 
+//   std::cout << "Line Three\n";
+//   return 0;
+// } // End Main Function
+}
